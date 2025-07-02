5c0faf8cdcc2722adb58823d766f9d016fa19867
Kenny Root <kenny@the-b.org>
2025-07-01 23:51:38 -0700

    Fix formatting

--- src/main.rs
+++ src/main.rs
@@ -46,8 +46,7 @@ const NET_FREQ: u32 = 40_000_000;
 
 type Spi2Bus = Mutex<NoopRawMutex, Spi<'static, Async>>;
 // type I2c0Bus = Mutex<NoopRawMutex, I2c<'static, Async>>;
-type EthernetSPI =
-    SpiDeviceWithConfig<'static, NoopRawMutex, Spi<'static, Async>, Output<'static>>;
+type EthernetSPI = SpiDeviceWithConfig<'static, NoopRawMutex, Spi<'static, Async>, Output<'static>>;
 
 // This creates a default app-descriptor required by the esp-idf bootloader.
 // For more information see: <https://docs.espressif.com/projects/esp-idf/en/stable/esp32/api-reference/system/app_image_format.html#application-description>
