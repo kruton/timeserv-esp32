dcd595265a11ca34b05e89524c8b19cebe200674
Kenny Root <kenny@the-b.org>
2025-07-08 20:29:54 -0700

    Split pmu into separate file

---
+++ src/pmu.rs
@@ -0,0 +1,54 @@
+use embedded_hal_async::delay::DelayNs;
+
+pub async fn m5sc2_init<I2C, E>(
+    axp: &mut axp192::Axp192<I2C>,
+    delay: &mut embassy_time::Delay,
+) -> Result<(), E>
+where
+    I2C: embedded_hal::i2c::I2c<Error = E>,
+{
+    // Default setup for M5Stack Core 2
+    axp.set_dcdc1_voltage(3350)?; // Voltage to provide to the microcontroller (this one!)
+
+    axp.set_ldo2_voltage(3300)?; // Peripherals (LCD, ...)
+    axp.set_ldo2_on(true)?;
+
+    axp.set_ldo3_voltage(2000)?; // Vibration motor
+    axp.set_ldo3_on(false)?;
+
+    axp.set_dcdc3_voltage(2800)?; // LCD backlight
+    axp.set_dcdc3_on(true)?;
+
+    axp.set_gpio1_mode(axp192::GpioMode12::NmosOpenDrainOutput)?; // Power LED
+    axp.set_gpio1_output(false)?; // In open drain modes, state is opposite to what you might
+                                  // expect
+
+    axp.set_gpio2_mode(axp192::GpioMode12::NmosOpenDrainOutput)?; // Speaker
+    axp.set_gpio2_output(true)?;
+
+    axp.set_key_mode(
+        // Configure how the power button press will work
+        axp192::ShutdownDuration::Sd4s,
+        axp192::PowerOkDelay::Delay64ms,
+        true,
+        axp192::LongPress::Lp1000ms,
+        axp192::BootTime::Boot512ms,
+    )?;
+
+    axp.set_gpio4_mode(axp192::GpioMode34::NmosOpenDrainOutput)?; // LCD reset control
+
+    axp.set_battery_voltage_adc_enable(true)?;
+    axp.set_battery_current_adc_enable(true)?;
+    axp.set_acin_current_adc_enable(true)?;
+    axp.set_acin_voltage_adc_enable(true)?;
+
+    // Actually reset the LCD
+    axp.set_gpio4_output(false)?;
+    axp.set_ldo3_on(true)?; // Buzz the vibration motor while intializing ¯\_(ツ)_/¯
+    delay.delay_ms(100u32).await;
+    axp.set_gpio4_output(true)?;
+    axp.set_ldo3_on(false)?;
+    delay.delay_ms(100u32).await;
+
+    Ok(())
+}
